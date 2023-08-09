5529e31d76880bb9a84446e96796f5a9640234da
Kono Steve <stevekono77@gmail.com>
2023-08-09 02:30:50 +0100

    Testing login and signup API routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
+const User = require('../models/userModel');
+
 // login user
 async function loginUser(req,res) {
+    console.log(req.body);
     res.json({ msg: 'Login user' });
 }
 
 // signup user
 async function signupUser(req,res) {
+    console.log(req.body);
     res.json({ msg: 'Signup user' });
 }
 
