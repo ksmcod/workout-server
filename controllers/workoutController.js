8b61fa500560d028bd5adb18c19318662f231321
Kono Steve <stevekono77@gmail.com>
2023-08-13 01:43:52 +0100

    getWorkouts controller only returns workouts for the authenticated user

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 
 // GET all workouts
 async function getWorkouts(req,res) {
-    const workouts = await Workout.find({}).sort({ createdAt: -1 });
+    const user_id = req.user._id;
+    const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 });
 
     res.status(200).json(workouts);
 }
